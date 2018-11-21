2aac68b4123f4c4b7cb2fb471ef3cc5f024207d3
John Grosh <john.a.grosh@gmail.com>
2018-11-21 18:43:27 -0500

    formatting

--- src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
+++ src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
@@ -44,13 +44,14 @@ public class OtherUtil
     {
         if(url==null)
             return null;
-        try {
+        try 
+        {
             URL u = new URL(url);
             URLConnection urlConnection = u.openConnection();
             urlConnection.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36");
             return urlConnection.getInputStream();
-        } catch(IOException|IllegalArgumentException e) {
         }
+        catch(IOException|IllegalArgumentException e) {}
         return null;
     }
     
