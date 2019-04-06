157df5b2f83e41179ff0002ca00ae46c7c84e3b3
Abady <gamersspeaks@gmail.com>
2019-04-06 16:58:33 +0300

    fix: start from 1

--- src/main/java/com/jagrosh/jmusicbot/utils/FormatUtil.java
+++ src/main/java/com/jagrosh/jmusicbot/utils/FormatUtil.java
@@ -41,7 +41,7 @@ public class FormatUtil {
     public static String progressBar(double percent)
     {
         String str = "";
-        for(int i=0; i<12; i++)
+        for(int i=1; i<12; i++)
             if(i == (int)(percent*12))
                 str+="\uD83D\uDD18"; // 🔘
             else
