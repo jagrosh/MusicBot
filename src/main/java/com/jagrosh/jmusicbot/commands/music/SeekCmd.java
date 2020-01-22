7aecc0c6ad5432e0da5d17a1090d168ba52d9f98
Whew., Inc <22574706+Whew-Inc@users.noreply.github.com>
2020-01-22 18:28:21 +0100

    Restructure if-statements

--- src/main/java/com/jagrosh/jmusicbot/commands/music/SeekCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/music/SeekCmd.java
@@ -46,61 +46,59 @@ public class SeekCmd extends MusicCommand
     public void doCommand(CommandEvent event)
     {
         AudioHandler handler = (AudioHandler) event.getGuild().getAudioManager().getSendingHandler();
-        if (handler.getPlayer().getPlayingTrack().isSeekable())
+        if (!handler.getPlayer().getPlayingTrack().isSeekable())
         {
-            AudioTrack currentTrack = handler.getPlayer().getPlayingTrack();
-            Settings settings = event.getClient().getSettingsFor(event.getGuild());
+            event.replyError("This track is not seekable.");
+            return;
+        }
 
-            if (!event.getMember().hasPermission(Permission.MANAGE_SERVER)
-                    && !event.getMember().getRoles().contains(settings.getRole(event.getGuild()))
-                    && currentTrack.getUserData(Long.class) != event.getAuthor().getIdLong())
-            {
-                event.replyError("You cannot seek **" + currentTrack.getInfo().title + "** because you didn't add it!");
-                return;
-            }
+        AudioTrack currentTrack = handler.getPlayer().getPlayingTrack();
+        Settings settings = event.getClient().getSettingsFor(event.getGuild());
 
-            String args = event.getArgs();
-            long track_duration = handler.getPlayer().getPlayingTrack().getDuration();
-            int seek_milliseconds = 0;
-            int seconds;
-            int minutes = 0;
-            int hours = 0;
+        if (!event.getMember().hasPermission(Permission.MANAGE_SERVER)
+                && !event.getMember().getRoles().contains(settings.getRole(event.getGuild()))
+                && currentTrack.getUserData(Long.class) != event.getAuthor().getIdLong())
+        {
+            event.replyError("You cannot seek **" + currentTrack.getInfo().title + "** because you didn't add it!");
+            return;
+        }
 
-            if (Pattern.matches("^([0-9]\\d):([0-5]\\d):([0-5]\\d)$", args))
-            {
-                hours = Integer.parseInt(args.substring(0, 2));
-                minutes = Integer.parseInt(args.substring(3, 5));
-                seconds = Integer.parseInt(args.substring(6));
-            }
-            else if (Pattern.matches("^([0-5]\\d):([0-5]\\d)$", args))
-            {
-                minutes = Integer.parseInt(args.substring(0, 2));
-                seconds = Integer.parseInt(args.substring(3, 5));
-            }
-            else if (Pattern.matches("^([0-5]\\d)$", args))
-            {
-                seconds = Integer.parseInt(args.substring(0, 2));
-            }
-            else
-            {
-                event.replyError("Invalid seek!");
-                return;
-            }
+        String args = event.getArgs();
+        long track_duration = handler.getPlayer().getPlayingTrack().getDuration();
+        int seek_milliseconds = 0;
+        int seconds;
+        int minutes = 0;
+        int hours = 0;
 
-            seek_milliseconds += hours * 3600000 + minutes * 60000 + seconds * 1000;
-            if (seek_milliseconds <= track_duration)
-            {
-                handler.getPlayer().getPlayingTrack().setPosition(seek_milliseconds);
-                event.replySuccess("Successfully seeked!");
-            }
-            else
-            {
-                event.replyError("Current track is not that long!");
-            }
+        if (Pattern.matches("^([0-9]\\d):([0-5]\\d):([0-5]\\d)$", args))
+        {
+            hours = Integer.parseInt(args.substring(0, 2));
+            minutes = Integer.parseInt(args.substring(3, 5));
+            seconds = Integer.parseInt(args.substring(6));
+        }
+        else if (Pattern.matches("^([0-5]\\d):([0-5]\\d)$", args))
+        {
+            minutes = Integer.parseInt(args.substring(0, 2));
+            seconds = Integer.parseInt(args.substring(3, 5));
+        }
+        else if (Pattern.matches("^([0-5]\\d)$", args))
+        {
+            seconds = Integer.parseInt(args.substring(0, 2));
         }
         else
         {
-            event.replyError("This track is not seekable.");
+            event.replyError("Invalid seek!");
+            return;
         }
+
+        seek_milliseconds += hours * 3600000 + minutes * 60000 + seconds * 1000;
+        if (seek_milliseconds > track_duration)
+        {
+            event.replyError("Current track is not that long!");
+            return;
+        }
+
+        handler.getPlayer().getPlayingTrack().setPosition(seek_milliseconds);
+        event.replySuccess("Successfully seeked!");
     }
 }
