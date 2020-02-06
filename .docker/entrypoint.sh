477ee7ccd1fd5e81db41103323312171fccf1c82
Carlos Feldmann <feldmannjunior@gmail.com>
2020-02-05 23:59:03 -0300

    Updated Dockerfile to build maven project Loading config from env variables

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,11 +1,75 @@
 #!/bin/bash
-cd /usr/app
-rm -rf config.txt
-if [ "$DISCORD_BOT_TOKEN" == "" ]; then
-    echo "Please provide the DISCORD_BOT_TOKEN env variable"
+serversettings=/MusicBot/serversettings.json
+config=/MusicBot/config.txt
+# Store a entry in the config
+# $1 = Config key
+# $2 = Env Variable
+function env_to_config() {
+  KEY="${2}"
+  if [[ ! -v $KEY ]]; then
+    echo "$KEY is not set, skipping"
+    return
+  fi
+  echo "$1 = \"${!KEY}\"" >> config.txt
+}
+# Load a config from the env variable $2
+# $1 Config key
+function load_from_env(){
+  KEY=${1}
+  env_to_config $KEY "CONFIG_${KEY^^}"
+}
+
+function load_from_env_required(){
+  KEY="CONFIG_${1^^}"
+  if [[ ! -v $KEY ]]; then
+    echo "$KEY is not set and is required"
     exit 1
+  fi
+  load_from_env "$1"
+}
+
+cd /usr/app
+# Setting configs
+if [ "$CONFIG_ENABLE" == "1" ];then
+  echo "Loading config from env variables"
+  rm -rf config.txt
+  # Setting playlist folder to volume location
+  echo "playlistsfolder = /MusicBot/Playlists" >> config.txt
+  # Loading user env variables
+  load_from_env_required token true
+  load_from_env_required owner
+  load_from_env prefix
+  load_from_env game
+  load_from_env songinstatus
+  load_from_env altprefix
+  load_from_env help
+  load_from_env stayinchannel
+  load_from_env maxtime
+  load_from_env updatealerts
+  load_from_env "eval"
+  env_to_config "lyrics.default" CONFIG_LYRICS_PROVIDER
+else
+  # If not using env variables to configure the bot, create a symlink to /MusicBot/config.txt file
+  if [ ! -f "$config" ]; then
+      if [ -f config.txt ]; then
+        # If config already exists, just copy it
+        cp config.txt $config
+      else
+        touch $config
+      fi
+  fi
+  # If the symlink don't exists create it
+  if [ ! -L config.txt ]; then
+        rm -f config.txt
+        echo "Creating config symlink"
+        ln -s $config /usr/app/config.txt
+      fi
 fi
-echo "token = \"${DISCORD_BOT_TOKEN}\"" >> config.txt
-echo "owner = \"${DISCORD_BOT_OWNER}\"" >> config.txt
-echo "prefix = \"${DISCORD_BOT_PREFIX}\"" >> config.txt
+
+# Check if serversettings exists, if not creates it
+if [ ! -f "$serversettings" ]; then
+  echo "{}" >> $serversettings
+fi
+
+# Starts the bot
 java -Dnogui=true -jar JMusicBot-*-All.jar
--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,29 @@
+FROM maven:3.6.3-jdk-8 as builder
+WORKDIR /build
+# Coping the pom file
+COPY pom.xml .
+# Download depedencies for cache
+RUN mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:go-offline -B
+# Go offline command doesn't download this artifact, so i am downloading manually
+RUN mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:get \
+        -DrepoUrl=https://jcenter.bintray.com/ \
+        -Dartifact=org.apache.maven.surefire:surefire-junit4:2.12.4
+# Coping source files
+COPY src/ /build/src
+# Building the project
+RUN mvn -o -B package
+
 FROM adoptopenjdk:8-jre-hotspot
 WORKDIR /usr/app
-COPY ./target/JMusicBot-*-All.jar .
-ENV DISCORD_BOT_TOKEN=""
-ENV DISCORD_BOT_OWNER="94934681706835968"
-ENV DISCORD_BOT_PREFIX="!"
+# If set to 0, disable config.txt writes
+ENV CONFIG_ENABLE 1
+# Coping the builded jars from the previous stage
+COPY --from=builder /build/target/JMusicBot-*-All.jar .
+# Declaring voluime
+VOLUME /MusicBot
+# Creating symlink of configuration to the volume
+RUN ln  -s /MusicBot/serversettings.json /usr/app/serversettings.json
+# Coping entrypoint
 COPY .docker/entrypoint.sh /entrypoint.sh
-VOLUME /usr/app/serversettings.json
+
 ENTRYPOINT ["/entrypoint.sh"]
