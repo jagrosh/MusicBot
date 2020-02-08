ac50ac2d036548e0e18d240de49a9e6fc762dbe8
Carlos Feldmann <feldmannjunior@gmail.com>
2020-02-08 18:34:12 -0300

    Always using /MusicBot/config.txt instead of switching based on env variable Adding missing new lines to the end of files

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
-SERVERSETTINGS=/MusicBot/serversettings.json
-PROVIDED_CONFIG=/MusicBot/config.txt
-
-# Config to be used in the bot
-CONFIG=/usr/app/config.txt
+# Directory to persist files
+PERSIST_DIR=/MusicBot
+# File to store server settings
+SERVER_SETTINGS="${PERSIST_DIR}/serversettings.json"
+# Config used in the bot
+CONFIG_FILE="${PERSIST_DIR}/config.txt"
+# Default config to be used in the bot
+DEFAULT_CONFIG=/usr/app/defaultConfig.txt
 
 cd /usr/app
-# Setting configs
-if [ "$CONFIG_ENABLE" == "1" ];then
-  # Using provided config using env variables
-  echo "Loading config from env variables"
-else
-  CONFIG=$PROVIDED_CONFIG
-  echo "Loading config from $PROVIDED_CONFIG"
+# If the persist dir doesn't exist create it
+if [ ! -d "${PERSIST_DIR}" ]; then
+  mkdir "$PERSIST_DIR"
+fi
+# Check if the config doesn't exist, and if doesn't copy the default one
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "Copping default config file to $CONFIG_FILE"
+  cp "$DEFAULT_CONFIG" "$CONFIG_FILE"
 fi
 
 # Check if serversettings exists, if not creates it
-if [ ! -f "$SERVERSETTINGS" ]; then
-  echo "{}" >> $SERVERSETTINGS
+if [ ! -f "$SERVER_SETTINGS" ]; then
+  echo "{}" >> "$SERVER_SETTINGS"
 fi
 
 # Starts the bot
-java -Dnogui=true -Dconfig="$CONFIG" -jar JMusicBot-*-All.jar
+java -Dnogui=true -Dconfig="$CONFIG_FILE" -jar JMusicBot-*-All.jar
