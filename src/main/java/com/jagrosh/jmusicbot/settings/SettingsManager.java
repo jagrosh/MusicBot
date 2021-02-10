8600c7c70875c679e050c8a9b0acbfdd8e59c7b6
Michaili K <mysteriouscursor+git@protonmail.com>
2021-02-10 12:05:41 +0100

    Rename repeat to repeat_mode

--- src/main/java/com/jagrosh/jmusicbot/settings/SettingsManager.java
+++ src/main/java/com/jagrosh/jmusicbot/settings/SettingsManager.java
@@ -42,23 +42,19 @@ public class SettingsManager implements GuildSettingsManager
             loadedSettings.keySet().forEach((id) -> {
                 JSONObject o = loadedSettings.getJSONObject(id);
 
-                // Legacy version support: On versions 0.2.7 and older, the repeat mode was represented as a boolean.
-                try
-                {
-                    if(o.getBoolean("repeat"))
-                        o.put("repeat", RepeatMode.On);
-                    else
-                        o.put("repeat", RepeatMode.Off);
-                } catch(JSONException e) { /* ignored */ }
+                // Legacy version support: On versions 0.3.3 and older, the repeat mode was represented as a boolean.
+                if (o.has("repeat") && o.getBoolean("repeat"))
+                    o.put("repeat_mode", RepeatMode.On);
+
 
                 settings.put(Long.parseLong(id), new Settings(this,
-                        o.has("text_channel_id") ? o.getString("text_channel_id")       : null,
-                        o.has("voice_channel_id")? o.getString("voice_channel_id")      : null,
-                        o.has("dj_role_id")      ? o.getString("dj_role_id")            : null,
-                        o.has("volume")          ? o.getInt("volume")                   : 100,
-                        o.has("default_playlist")? o.getString("default_playlist")      : null,
-                        o.has("repeat")          ? o.getEnum(RepeatMode.class, "repeat"): RepeatMode.Off,
-                        o.has("prefix")          ? o.getString("prefix")                : null));
+                        o.has("text_channel_id") ? o.getString("text_channel_id")            : null,
+                        o.has("voice_channel_id")? o.getString("voice_channel_id")           : null,
+                        o.has("dj_role_id")      ? o.getString("dj_role_id")                 : null,
+                        o.has("volume")          ? o.getInt("volume")                        : 100,
+                        o.has("default_playlist")? o.getString("default_playlist")           : null,
+                        o.has("repeat_mode")     ? o.getEnum(RepeatMode.class, "repeat_mode"): RepeatMode.Off,
+                        o.has("prefix")          ? o.getString("prefix")                     : null));
             });
         } catch(IOException | JSONException e) {
             LoggerFactory.getLogger("Settings").warn("Failed to load server settings (this is normal if no settings have been set yet): "+e);
@@ -104,7 +100,7 @@ public class SettingsManager implements GuildSettingsManager
             if(s.getDefaultPlaylist() != null)
                 o.put("default_playlist", s.getDefaultPlaylist());
             if(s.getRepeatMode()!=RepeatMode.Off)
-                o.put("repeat", s.getRepeatMode());
+                o.put("repeat_mode", s.getRepeatMode());
             if(s.getPrefix() != null)
                 o.put("prefix", s.getPrefix());
             obj.put(Long.toString(key), o);
