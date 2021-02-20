675bedaf310f02ad01ed5434586ee4afeb90f6f2
SplitPixl <splitpixl@gmail.com>
2021-02-20 14:44:02 -0800

    more

--- src/main/java/com/jagrosh/jmusicbot/audio/filter/KaraokeConfig.kt
+++ src/main/java/com/jagrosh/jmusicbot/audio/filter/KaraokeConfig.kt
@@ -1,4 +1,20 @@
 package com.jagrosh.jmusicbot.audio.filter
 
-class KaraokeConfig {
+import com.github.natanbc.lavadsp.karaoke.KaraokePcmAudioFilter
+import com.jagrosh.jmusicbot.audio.filter.AudioFilterConfig
+import com.sedmelluq.discord.lavaplayer.filter.AudioFilter
+import com.sedmelluq.discord.lavaplayer.filter.UniversalPcmAudioFilter
+import com.sedmelluq.discord.lavaplayer.format.AudioDataFormat
+
+class KaraokeConfig : AudioFilterConfig() {
+    override fun build(format: AudioDataFormat, output: UniversalPcmAudioFilter): AudioFilter {
+        return KaraokePcmAudioFilter(output, format.channelCount, format.sampleRate)
+    }
+
+    override fun getDescription(): String {
+        return String.format("Karaoke")
+    }
+
+    init {
+    }
 }
