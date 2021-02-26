e297ef86db232daa080ac8fa32af2f64f90b284c
chr-ibb L <46900764+chr-ibb@users.noreply.github.com>
2021-02-25 18:20:25 -0800

    add license header, add author tag, fix inconsistent formatting

--- src/main/java/com/jagrosh/jmusicbot/commands/dj/NextCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/dj/NextCmd.java
@@ -1,24 +1,38 @@
+/*
+ * Copyright 2018 John Grosh <john.a.grosh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jagrosh.jmusicbot.commands.dj;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jmusicbot.Bot;
 import com.jagrosh.jmusicbot.audio.AudioHandler;
-import com.jagrosh.jmusicbot.audio.QueuedTrack;
 import com.jagrosh.jmusicbot.commands.DJCommand;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
 import net.dv8tion.jda.api.entities.User;
 
 /**
- * DJ Command for skipping a song without removing it from the repeating queue
- * https://github.com/chr-ibb
+ *
+ * @author Chris Ibbotson <chr.ibbotson@gmail.com>
  */
-public class NextCmd extends DJCommand {
+public class NextCmd extends DJCommand
+{
     public NextCmd(Bot bot)
     {
         super(bot);
         this.name = "next";
-        this.help = "skips the current song. If we're in repeat mode, re-add it to the queue";
+        this.help = "skips the current song without removing it from a repeating queue";
         this.aliases = bot.getConfig().getAliases(this.name);
         this.bePlaying = true;
     }
