d5b99032f85e0afa5a9d0b7ccb25f9ca07586f63
Daniel <dippigames@gmail.com>
2021-03-19 04:08:29 +0100

    Added TILII command

--- src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
+++ src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
@@ -110,6 +110,7 @@ public class JMusicBot
                         new SCSearchCmd(bot),
                         new ShuffleCmd(bot),
                         new SkipCmd(bot),
+                        new TiliiCmd(bot),
 
                         new ForceRemoveCmd(bot),
                         new ForceskipCmd(bot),
