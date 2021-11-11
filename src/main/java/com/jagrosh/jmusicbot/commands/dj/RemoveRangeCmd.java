fb39d26c6524b180afad34acf1037263869c6326
michaeltwang <wangmichael2017@gmail.com>
2021-11-11 02:26:25 -0500

    Fix most PMD violations

--- src/main/java/com/jagrosh/jmusicbot/commands/dj/RemoveRangeCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/dj/RemoveRangeCmd.java
@@ -6,19 +6,17 @@ import com.jagrosh.jmusicbot.audio.AudioHandler;
 import com.jagrosh.jmusicbot.audio.QueuedTrack;
 import com.jagrosh.jmusicbot.commands.DJCommand;
 import com.jagrosh.jmusicbot.queue.FairQueue;
-import com.jagrosh.jmusicbot.settings.Settings;
-import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.User;
 
 /**
- * Command that provides users the ability to remove a range of songs in the queue.
+ * Command that provides users the ability to remove a range of songs in
+ * the queue.
  */
 public class RemoveRangeCmd extends DJCommand 
 {
     /** 
     * RemoveRangeCmd Class constructor.
     */
-    public RemoveRangeCmd(Bot bot)
+    public RemoveRangeCmd(final Bot bot)
     {
         super(bot);
         this.name = "removerange";
@@ -36,30 +34,51 @@ public class RemoveRangeCmd extends DJCommand
      * @param event the event that is being handled
      */
     @Override
-    public void doCommand(CommandEvent event) 
+    public void doCommand(final CommandEvent event) 
     {
-        AudioHandler handler = (AudioHandler)event.getGuild().getAudioManager().getSendingHandler();
-        FairQueue<QueuedTrack> queue = handler.getQueue();
+        final AudioHandler handler = (AudioHandler)event.getGuild().getAudioManager().getSendingHandler();
+        final FairQueue<QueuedTrack> queue = handler.getQueue();
         if(queue.isEmpty())
         {
             event.replyError("There is nothing in the queue!");
             return;
         }
-        int from;
-        int to;
 
-        String[] parts = event.getArgs().split("\\s+", 2);
-        if(parts.length < 2)
+        final String[] parts = event.getArgs().split("\\s+", 2);
+
+        if(parts.length < this.arguments.split("\\s+").length)
         {
             event.replyError("Please include two valid indexes.");
             return;
         }
         
+        int fromIndex;
+        int toIndex;
+        
         try
         {
             // Validate the args
-            from = Integer.parseInt(parts[0]);
-            to = Integer.parseInt(parts[1]);
+            fromIndex = Integer.parseInt(parts[0]);
+            toIndex = Integer.parseInt(parts[1]);        
+            if (fromIndex > toIndex)
+            {
+            	event.replyError("From position cannot be greater than the to position.");
+            	return;
+            }
+
+            // Validate that from and to are available
+            if (isUnavailablePosition(queue, fromIndex))
+            {
+            	final String reply = String.format("`%d` is not a valid position in the queue!", fromIndex);
+            	event.replyError(reply);
+            	return;
+            }
+            if (isUnavailablePosition(queue, toIndex))
+            {
+            	final String reply = String.format("`%d` is not a valid position in the queue!", toIndex);
+            	event.replyError(reply);
+            	return;
+            }
         }
         catch (NumberFormatException e)
         {
@@ -67,28 +86,8 @@ public class RemoveRangeCmd extends DJCommand
             return;
         }
 
-        if (from > to)
-        {
-            event.replyError("From position cannot be greater than the to position.");
-            return;
-        }
-
-        // Validate that from and to are available
-        if (isUnavailablePosition(queue, from))
-        {
-            String reply = String.format("`%d` is not a valid position in the queue!", from);
-            event.replyError(reply);
-            return;
-        }
-        if (isUnavailablePosition(queue, to))
-        {
-            String reply = String.format("`%d` is not a valid position in the queue!", to);
-            event.replyError(reply);
-            return;
-        }
-      
-        handler.getQueue().removeRange(from, to);
-        String reply = String.format("Removed all songs from the queue from position `%d` to `%d`.", from, to);
+        queue.removeRange(fromIndex, toIndex);
+        final String reply = String.format("Removed all songs from the queue from position `%d` to `%d`.", fromIndex, toIndex);
         event.replySuccess(reply);
     }
 
@@ -101,8 +100,8 @@ public class RemoveRangeCmd extends DJCommand
      * @param position the position to be checked
      * @return boolean true if the position is valid, else false
      */
-    private static boolean isUnavailablePosition(FairQueue<QueuedTrack> queue, int position)
+    private static boolean isUnavailablePosition(final FairQueue<QueuedTrack> queue, final int position)
     {
-        return (position < 1 || position > queue.size());
+        return position < 1 || position > queue.size();
     }
 }
