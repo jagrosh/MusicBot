1f81256d12b999e13aeb1e633e4274240d31f93e
nicolas <nicolas.ziegle@logifleet.ch>
2023-04-27 11:47:54 +0200

    Added shortcut logic 2

--- src/main/java/com/jagrosh/jmusicbot/BotConfig.java
+++ src/main/java/com/jagrosh/jmusicbot/BotConfig.java
@@ -19,12 +19,15 @@ import com.jagrosh.jmusicbot.entities.Prompt;
 import com.jagrosh.jmusicbot.utils.FormatUtil;
 import com.jagrosh.jmusicbot.utils.OtherUtil;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import com.typesafe.config.*;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigException;
+import com.typesafe.config.ConfigFactory;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import net.dv8tion.jda.api.OnlineStatus;
-import net.dv8tion.jda.api.entities.Activity;
 
 /**
  * 
@@ -46,6 +49,7 @@ public class BotConfig
     private OnlineStatus status;
     private Activity game;
     private Config aliases, transforms;
+    private Config shortcuts;
 
     private boolean valid = false;
     
@@ -90,6 +94,7 @@ public class BotConfig
             aloneTimeUntilStop = config.getLong("alonetimeuntilstop");
             playlistsFolder = config.getString("playlistsfolder");
             aliases = config.getConfig("aliases");
+            shortcuts = config.getConfig("shortcuts");
             transforms = config.getConfig("transforms");
             dbots = owner == 113156185389092864L;
             
@@ -344,7 +349,11 @@ public class BotConfig
             return new String[0];
         }
     }
-    
+
+    public Config getAllShortcuts() {
+            return shortcuts;
+    }
+
     public Config getTransforms()
     {
         return transforms;
