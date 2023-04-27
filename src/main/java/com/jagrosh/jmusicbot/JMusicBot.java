d1a19a075d256fc208c5273c33c6cd0d0d3e3177
nicolas <nicolas.ziegle@logifleet.ch>
2023-04-27 12:59:58 +0200

    fixed missing command for custom option

--- src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
+++ src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
@@ -110,6 +110,7 @@ public class JMusicBot
                         new LyricsCmd(bot),
                         new NowplayingCmd(bot),
                         new PlayCmd(bot),
+                        new CustomPlayCmd(bot),
                         new PlaylistsCmd(bot),
                         new QueueCmd(bot),
                         new RemoveCmd(bot),
