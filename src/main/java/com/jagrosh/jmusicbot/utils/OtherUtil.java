13225701e9498d01b19220964ebbbb7aab7d5d7e
Ntdi <yogikngyt@gmail.com>
2024-03-17 12:57:28 -0400

    Update OtherUtil.java

--- src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
+++ src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
@@ -218,13 +218,6 @@ public class OtherUtil
      */
     public static String getUnsupportedBotReason(JDA jda) 
     {
-        if (jda.getSelfUser().getFlags().contains(User.UserFlag.VERIFIED_BOT))
-            return "The bot is verified. Using JMusicBot in a verified bot is not supported.";
-
-        ApplicationInfo info = jda.retrieveApplicationInfo().complete();
-        if (info.isBotPublic())
-            return "\"Public Bot\" is enabled. Using JMusicBot as a public bot is not supported. Please disable it in the Developer Dashboard.";
-
         return null;
     }
 }
