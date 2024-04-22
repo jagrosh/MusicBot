b42094a3b034bbb3d5cf476876356af25a79743a
Michail <git@michaili.dev>
2024-04-22 21:39:20 +0200

    Switch to new lavalink's new YouTube audio source manager

--- src/main/java/com/jagrosh/jmusicbot/audio/PlayerManager.java
+++ src/main/java/com/jagrosh/jmusicbot/audio/PlayerManager.java
@@ -20,7 +20,7 @@ import com.jagrosh.jmusicbot.Bot;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
-import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
+import dev.lavalink.youtube.YoutubeAudioSourceManager;
 import net.dv8tion.jda.api.entities.Guild;
 
 /**
@@ -39,10 +39,10 @@ public class PlayerManager extends DefaultAudioPlayerManager
     public void init()
     {
         TransformativeAudioSourceManager.createTransforms(bot.getConfig().getTransforms()).forEach(t -> registerSourceManager(t));
+        registerSourceManager(new YoutubeAudioSourceManager(true));
         AudioSourceManagers.registerRemoteSources(this);
         AudioSourceManagers.registerLocalSource(this);
         DuncteBotSources.registerAll(this, "en-US");
-        source(YoutubeAudioSourceManager.class).setPlaylistPageCount(10);
     }
     
     public Bot getBot()
