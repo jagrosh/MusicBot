8b950e79827bea84b7caf13ea95eef0992779395
lillie™ <66176496+lilliepad1@users.noreply.github.com>
2024-07-10 15:39:05 -0400

    make version check point towards fork repo

--- src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
+++ src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
@@ -43,7 +43,7 @@ public class OtherUtil
     public final static String NEW_VERSION_AVAILABLE = "There is a new version of JMusicBot available!\n"
                     + "Current version: %s\n"
                     + "New Version: %s\n\n"
-                    + "Please visit https://github.com/jagrosh/MusicBot/releases/latest to get the latest release.";
+                    + "Please visit https://github.com/lilliepad1/MusicBotShiverFork/releases/latest to get the latest release.";
     private final static String WINDOWS_INVALID_PATH = "c:\\windows\\system32\\";
     
     /**
@@ -188,7 +188,7 @@ public class OtherUtil
         try
         {
             Response response = new OkHttpClient.Builder().build()
-                    .newCall(new Request.Builder().get().url("https://api.github.com/repos/jagrosh/MusicBot/releases/latest").build())
+                    .newCall(new Request.Builder().get().url("https://api.github.com/repos/lilliepad1/MusicBot/MusicBotShiverFork/latest").build())
                     .execute();
             ResponseBody body = response.body();
             if(body != null)
